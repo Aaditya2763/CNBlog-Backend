8af207122082d926b2f1b4672b50459d2c5ea778
Aditya2763 <singhaditya2763@gmail.com>
2023-06-22 08:15:49 +0530

    update

--- app.js
+++ app.js
@@ -17,35 +17,44 @@ const db_Url=process.env.DB_URL;
 //seeding data from dummy_Quotes
 // seedDb()
 
-
-app.get('/hello',(req,res)=>{
-res.status(200).json({msg:'hello from server'});
-})
-
-
-app.use(cors({
-//     origin:[
-// //         `https://aditya-react-blog-website.vercel.app/`
-// // `http://localhost:3000/`
-// //       `*`
-//     ],
-    
-}));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow the specified HTTP methods
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow the specified headers
-    next();
-  });
-  
-
-
 app.use(express.urlencoded({extended:false}));
 //it will work as pody-parser or urlencoder
 app.use(express.json())
 app.use(methodOverride('_method'))
-app.use(quotesRoutes);
+
+app.use(
+  cors({
+    origin: [
+    //   `${process.env.DEVELOPMENT}`,
+    //   `${process.env.CLIENT_URL}`,
+    //   `${process.env.ADMIN_URL}`,
+      `http://localhost:3000/`
+      `https://aditya-react-blog-website.vercel.app/`
+    ],
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+
+// app.use(cors({
+//     //     origin:[
+//     // //         `https://aditya-react-blog-website.vercel.app/`
+//     // // `http://localhost:3000/`
+//     // //       `*`
+//     //     ],
+        
+//     }));
+    
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow the specified HTTP methods
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow the specified headers
+        next();
+      });
+
+      app.use(quotesRoutes);
+      app.get('/hello',(req,res)=>{
+        res.status(200).json({msg:'hello from server'});
+        })
 
 
 const port=process.env.port || 8080;
