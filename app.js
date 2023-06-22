7310f1b4b9cfc865fb595fe991ad94e86c029622
Aditya2763 <singhaditya2763@gmail.com>
2023-06-22 08:28:27 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
- if(process.env.Node_Env !=='production'){
-    require('dotenv').config();
-}
+ 
  const express=require('express');
  const app=express();
+ const cors = require("cors");
+require("dotenv").config();
  const mongoose=require('mongoose')
 const quotesRoutes=require('./api/quotesRoutes')
 const seedDb=require('./seed')
@@ -31,30 +31,44 @@ app.use(
       "http://localhost:3000",
       "https://aditya-react-blog-website.vercel.app"
     ],
+    credentials: true,
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
 );
 
-// app.use(cors({
-//     //     origin:[
-//     // //         `https://aditya-react-blog-website.vercel.app/`
-//     // // `http://localhost:3000/`
-//     // //       `*`
-//     //     ],
-        
-//     }));
-    
-    app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow the specified HTTP methods
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow the specified headers
-        next();
-      });
 
-      app.use(quotesRoutes);
-      app.get('/hello',(req,res)=>{
-        res.status(200).json({msg:'hello from server'});
-        })
+// app.use(
+//     cors({
+//       origin: [
+//         `${process.env.CLIENT_URL}`,
+//         `${process.env.DEVELOPMENT}`,
+//         `${process.env.ADMIN_URL}`,
+//       ],
+//       credentials: true,
+//     })
+//   );
+  app.use((req, res, next) => {
+    const allowedOrigins = [
+      `${process.env.CLIENT_URL}`,
+      `${process.env.DEVELOPMENT}`,
+      `${process.env.ADMIN_URL}`,
+    ];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    // res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
+    // res.header("Access-Control-Allow-Origin", `${process.env.HOME_URL}`)
+    // res.header("Access-Control-Allow-Origin", `${process.env.HOME_URL2}`)
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
+  
 
 
 const port=process.env.port || 8080;
